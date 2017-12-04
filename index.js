0c3e5cef4a92c03ddca9d13a52d1ea9845549b7e
Jamie Syme <jamie.d.syme@gmail.com>
2017-12-04 12:48:18 -0800

    Supported ls with multiple tags. Cleaned code. Added bloom filters per tag.

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 const Program = require('commander');
 const Commands = require('./lib/commands');
 
+function withErrors(cb) {
+	return async (...args) => {
+		try {
+			await cb(...args);
+		} catch (e) {
+			console.log(e);
+			process.exit(1);
+		}
+	}
+}
+
 Program.version('0.1.0');
 
 Program
 	.command('add <files...>')
 	.option('-t, --tags <tags...>', 'tags to attach')
-	.action((env, options) => {
-		Commands.add(env, options.tags);
-	});
+	.action(
+		withErrors(
+			async (env, options) => {
+				await Commands.add(env, options.tags);
+			}
+		)
+	);
 
 Program
 	.command('ls [tags...]')
-	.action((env, options) => {
-		Commands.ls(env);
-	});
+	.action(
+		withErrors(
+			async (env, options) => {
+				await Commands.ls(env);
+			}
+		)
+	);
 
 Program.parse(process.argv);
