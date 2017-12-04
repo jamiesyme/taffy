70ec56e528e9a10094c4b93ff8cf912d69796991
Jamie Syme <jamie.d.syme@gmail.com>
2017-12-03 21:22:09 -0800

    Started implementation for `taffy add ...`.

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const Commands = require('./lib/commands');
 Program.version('0.1.0');
 
 Program
+	.command('add <files...>')
+	.option('-t, --tags <tags...>', 'tags to attach')
+	.action((env, options) => {
+		Commands.add(env, options.tags);
+	});
+
+Program
 	.command('ls [tags...]')
 	.action((env, options) => {
 		Commands.ls(env);
