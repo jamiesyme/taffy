44971bb9736011d17973a2dd1db3879fd475d0f8
Jamie Syme <jamie.d.syme@gmail.com>
2018-01-08 10:54:47 -0800

    Removed commander to clean up help output.

--- index.js
+++ index.js
@@ -1,39 +1,79 @@
 const Program = require('commander');
 const Commands = require('./lib/commands');
 
-function withErrors(cb) {
-	return async (...args) => {
-		try {
-			await cb(...args);
-		} catch (e) {
-			console.log(e);
-			process.exit(1);
-		}
-	}
+function help () {
+	console.log('');
+	console.log('  Usage:');
+	console.log('    taffy add [--tags <tag>...] <file>...');
+	console.log('    taffy ls [--no-bloom] [<tag>...]');
+	console.log('    taffy -h | --help');
+	console.log('    taffy --version');
+	console.log('');
+	console.log('  Options:');
+	console.log('    -h --help   Show this screen.');
+	console.log('    --version   Show version.');
+	console.log('    --tags      Tags for new file(s).');
+	console.log('    --no-bloom  Disable bloom filter optimization.');
+	console.log('');
+	process.exit(1);
 }
 
-Program.version('0.1.0');
+function version () {
+	console.log('0.1.0');
+	process.exit(0);
+}
 
-Program
-	.command('add <files...>')
-	.option('-t, --tags <tags...>', 'tags to attach')
-	.action(
-		withErrors(
-			async (env, options) => {
-				await Commands.add(env, options.tags);
-			}
-		)
-	);
+async function add (args) {
+	let tags = [];
+	let files = [];
+	if (args.length > 0) {
+		if (args[0].match(/^--tags$/)) {
+			tags = args[1].split(' ');
+			args = args.slice(2);
+		} else if (args[0].match(/^--tags=/)) {
+			tags = args[0].split('=', 2)[1].split(' ');
+			args = args.slice(1);
+		}
+	}
+	files = args;
+	await Commands.add(files, tags);
+	process.exit(0);
+}
 
-Program
-	.command('ls [tags...]')
-	.option('--no-bloom', 'do not use the bloom filter optimization')
-	.action(
-		withErrors(
-			async (env, options) => {
-				await Commands.ls(env, options.bloom);
-			}
-		)
-	);
+async function ls (args) {
+	let tags = [];
+	let bloom = true;
+	if (args.length > 0) {
+		if (args[0].match(/^--no-bloom$/)) {
+			bloom = false;
+			args = args.slice(1);
+		}
+	}
+	tags = args;
+	await Commands.ls(tags, bloom);
+	process.exit(0);
+}
 
-Program.parse(process.argv);
+async function main () {
+	let args = process.argv.slice(2);
+	switch (args[0]) {
+	case 'add':
+		await add(args.slice(1));
+		break;
+
+	case 'ls':
+		await ls(args.slice(1));
+		break;
+
+	case '--version':
+		version();
+		break;
+
+	case '-h':
+	case '--help':
+	default:
+		help();
+		break;
+	}
+}
+main();
